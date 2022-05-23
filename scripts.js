1b4306156146dc0c53ae4583661abe7a7c462400
SamSvetlik <sam.svetlik@gmail.com>
2022-05-22 20:16:18 -0500

    final

--- scripts.js
+++ scripts.js
@@ -13,6 +13,16 @@
         "title": "delectus aut autem",
         "completed": false
     }]
+
+    let currentMarker = "complete"
+
+    const changeMarker = () => {
+        if(currentMarker === "complete"){
+          currentMarker = "incomplete"
+        } else {
+          currentMarker = "complete"
+        }
+      }
     
     const fetchTodos = () => {
         fetch('https://jsonplaceholder.typicode.com/todos')
@@ -61,4 +71,43 @@
                 parent.removeChild(parent.lastChild);
             }
     }
-    
+    const completedOrNot = () => {
+        if (currentMarker === "complete") {
+            console.log(currentMarker)
+            changeMarker()
+        const newLi = document.createElement("li")
+        const completed = document.createTextNode("The following have been completed:")
+        newLi.appendChild(completed)
+        const findOL = document.getElementById("todo-list")
+        findOL.appendChild(newLi)
+        const completeTrue = arrayOfTodos.filter((item) => item.completed === true)
+        for (let i = 0; i < completeTrue.length; i++) {
+            console.log(i+1, completeTrue[i].title)
+            const newLi = document.createElement("li")
+            const content = document.createTextNode(completeTrue[i].id +" " + completeTrue[i].title)
+            newLi.appendChild(content)
+            const findOL = document.getElementById("todo-list")
+            findOL.appendChild(newLi)
+            }
+            }
+        else {
+            console.log (currentMarker)
+            changeMarker()
+        const newLi = document.createElement("li")
+        const notCompleted = document.createTextNode("The following have not been completed:")
+        newLi.appendChild(notCompleted)
+        const findOL = document.getElementById("todo-list")
+        findOL.appendChild(newLi)
+        const completeFalse = arrayOfTodos.filter((item) => item.completed === false)
+        for (let i = 0; i < completeFalse.length; i++) {
+            console.log(i+1, completeFalse[i].title)
+            const newLi = document.createElement("li")
+            const content = document.createTextNode(completeFalse[i].id + " " + completeFalse[i].title)
+            newLi.appendChild(content)
+            const findOL = document.getElementById("todo-list")
+            findOL.appendChild(newLi)
+
+        
+            }
+        }
+    }
